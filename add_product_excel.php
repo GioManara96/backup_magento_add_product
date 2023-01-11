afb0d15d8de4b9d2b13a2cec0edebff661295e3c
Giovanni <giomanara1996@gmail.com>
2023-01-11 12:00:20 +0100

    live ver-1.0

--- add_product_excel.php
+++ add_product_excel.php
@@ -1,7 +1,7 @@
 <?php
 
 require "vendor/autoload.php";
-
+require "functions.php";
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
 $reader = new Xlsx;
@@ -23,55 +23,4 @@ foreach ($data as $i => $record) {
     }
 }
 
-$postfields = array("items" => array());  // array dei postfields finale
-
-// ciclo per entrare nell'array degli sku del main
-for ($i = 0; $i < count($main_skus); $i++) {
-    // ciclo per accedere effettivamente agli sku nell'array dei related
-    foreach ($related_skus[$i] as $related) {
-        // postfield con dentro tutti gli items associati ad 1 prodotto main
-        $postfield = array(
-            "sku" => $main_skus[$i],
-            "linkType" => 'related',
-            "linkedProductSku" => $related,
-            "linkedProductType" => "simple",
-            "position" => 0
-        );
-        array_push($postfields["items"], $postfield);
-    }
-
-    /**
-     * una volta finito di caricare i postfileds per un main product
-     * eseguo la chiamata POST e successivamente re-inizializzo l'array postfields.
-     * Per evitare chiamate inutili con array vuoti inserisco questo if.
-     */
-
-    // $curl = curl_init();
-
-    // // file di testo da cui prendo il il bearer token
-    // $fileToken = fopen("token.txt", "r");
-    // $auth = fgets($fileToken);
-
-    // curl_setopt_array($curl, array(
-    //     CURLOPT_URL => 'https://dominio.com/rest/V1/products/' . $main . '/links',
-    //     CURLOPT_RETURNTRANSFER => true,
-    //     CURLOPT_ENCODING => '',
-    //     CURLOPT_MAXREDIRS => 10,
-    //     CURLOPT_TIMEOUT => 0,
-    //     CURLOPT_FOLLOWLOCATION => true,
-    //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    //     CURLOPT_CUSTOMREQUEST => 'POST',
-    //     CURLOPT_POSTFIELDS => json_encode($postfields),
-    //     CURLOPT_HTTPHEADER => array(
-    //         $auth,
-    //         'Cookie: PHPSESSID=s88lk5blk224u8e6jlgjdpaedc'
-    //     ),
-    // ));
-
-    // $response = curl_exec($curl);
-
-    // curl_close($curl);
-    // fclose($fileToken);
-    echo "Ciclo $i\n" . json_encode($postfields, JSON_PRETTY_PRINT) . "\n";
-    $postfields = array("items" => array());
-}
+add_related_products($main_skus, $related_skus);
